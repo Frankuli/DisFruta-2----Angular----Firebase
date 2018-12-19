8083953bb9ff6a743617e8ab32a4f38a727115ec
Franco <franco.mana@hotmail.com>
2018-12-19 17:57:54 -0300

    estado envio

--- src/app/models/send.ts
+++ src/app/models/send.ts
@@ -2,9 +2,11 @@ export class Send {
   cadete: string;
   destino: string;
   cliente: string;
+  estado: string;
 
   constructor(userId: string, public envio: any, public sucursal) {
     this.cadete = '';
+    this.estado = 'Pendiente';
     this.cliente= userId;
   }
 }
